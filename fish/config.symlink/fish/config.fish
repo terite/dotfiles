2ea92d2095233f7b27ee747712e73792d3162720
David Stensland <me@terite.com>
2014-12-23 18:18:32 -0500

    only load the config files we need

--- fish/config.symlink/fish/config.fish
+++ fish/config.symlink/fish/config.fish
@@ -1,17 +1,10 @@
-# Avoid an infinite loop
-if set -q DOTFILES;
-    exit;
-end
-
 set DOTFILES (abspath (dirname (status -f))"/../../..")
-for i in $DOTFILES/**/*.fish;
-    source $i;
+
+for file in $DOTFILES/*/*.fish;
+    source $file;
 end
 
-# TODO: These should be caught by the above loop. They're not for some reason
 set -g VIRTUALFISH_COMPAT_ALIASES
 source $DOTFILES/fish/virtualfish/virtual.fish
 source $DOTFILES/fish/virtualfish/auto_activation.fish
 source $DOTFILES/fish/virtualfish/global_requirements.fish
-
-set fish_greeting ""
