9247f083617777ff112fab41f9af32a06da1be7a
David Stensland <me@terite.com>
2014-12-23 18:14:50 -0500

    teach vim to behave better around fish

--- vim/mvim.fish
+++ vim/mvim.fish
@@ -1,2 +1,5 @@
 set -l mvim_vim "/Applications/MacVim.app/Contents/MacOS/Vim"
-if test -e $mvim_vim; alias vim $mvim_vim; end
+if test -e $mvim_vim;
+    alias vim $mvim_vim;
+    alias vi $mvim_vim;
+end
