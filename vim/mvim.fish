12245b02b434c89815ebf44502109992677dd6bd
David Stensland <me@terite.com>
2015-03-10 10:28:15 -0700

    use nvim, and remove the invalid gvimrc link

--- vim/mvim.fish
+++ vim/mvim.fish
@@ -1,5 +1,10 @@
 set -l mvim_vim "/Applications/MacVim.app/Contents/MacOS/Vim"
-if test -e $mvim_vim;
+
+# prefer neovom > macvim > vim
+if which nvim 1>/dev/null;
+    alias vim nvim;
+    alias vi nvim;
+else if test -e $mvim_vim;
     alias vim $mvim_vim;
     alias vi $mvim_vim;
 end
