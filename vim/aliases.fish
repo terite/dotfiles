7dc402c94d38a1b679e5103fc8bf87d238729169
David Stensland <me@terite.com>
2015-03-16 09:56:25 -0700

    vim aliases, add output to the vimout file

--- vim/aliases.fish
+++ vim/aliases.fish
@@ -1,10 +1,12 @@
 set -l mvim_vim "/Applications/MacVim.app/Contents/MacOS/Vim"
+set -l vim_cmd "vim"
 
 # prefer neovom > macvim > vim
 if which nvim 1>/dev/null;
-    alias vim nvim;
-    alias vi nvim;
+    set -l vim_cmd nvim
 else if test -e $mvim_vim;
-    alias vim $mvim_vim;
-    alias vi $mvim_vim;
+    set -l vim_cmd $mvim_vim
 end
+
+alias vim "$vim_cmd -w ~/vimout.txt"
+alias vi "$vim_cmd -w ~/vimout.txt"
