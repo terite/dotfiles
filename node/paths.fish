31f965ee3ba69853d3e0ea5a44dba5b38ea913f6
David Stensland <me@terite.com>
2019-01-11 11:36:43 -0800

    hack to prefer node8 if it exists

--- node/paths.fish
+++ node/paths.fish
@@ -1,3 +1,8 @@
+set NODE8_BIN "/usr/local/opt/node@8/bin"
+if test -d "$NODE8_BIN"
+    set PATH "$NODE8_BIN" $PATH
+end
+
 if test -d "$HOME/.npm-packages/bin"
     set PATH "$HOME/.npm-packages/bin" $PATH
 end
