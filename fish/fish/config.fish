c9617cd10ebd488ad8d36b126285620c2cee550b
David Stensland <me@terite.com>
2019-01-11 11:37:19 -0800

    remove virtualfish support for now
    I dont use virtualenv as much, so now that this is broken I will just
    disable it instead of fixing it

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -8,10 +8,5 @@ for file in $DOTFILES/*/*.fish;
     . $file;
 end
 
-set -g VIRTUALFISH_COMPAT_ALIASES
-. $DOTFILES/fish/virtualfish/virtual.fish
-. $DOTFILES/fish/virtualfish/auto_activation.fish
-. $DOTFILES/fish/virtualfish/global_requirements.fish
-
 # test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 alias imgcat=~/.iterm2/imgcat; alias it2dl=~/.iterm2/it2dl
