f7e11490bc03cad5d7be84d3ddb49d7e25925ab1
David Stensland <me@terite.com>
2019-02-23 22:19:09 -0800

    cleanup paths

--- system/paths.fish
+++ system/paths.fish
@@ -1,31 +1,15 @@
 function _add_path
-    set trypath $argv[1]
-    for p in $PATH
-        if [ $p = $trypath ]
-            # already in $PATH
-            return 0
-        end
+    set -l trypath $argv[1]
+
+    if contains -- $trypath $fish_user_paths
+        return 0
     end
 
     if test -d $trypath
-        set PATH $PATH $trypath
+        set --global --export --prepend fish_user_paths $trypath
     end
 
 end
 
 _add_path "$DOTFILES/bin"
-_add_path "$HOME/bin"
 _add_path "$HOME/local/bin"
-_add_path "/Applications/Postgres.app/Contents/Versions/latest/bin"
-
-# system python & pip
-# _add_path "$HOME/Library/Python/2.7/bin"
-
-# macports python & pip
-_add_path "/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin"
-
-# macports
-_add_path "/opt/local/bin"
-
-# homebrew
-_add_path "/usr/local/sbin"
