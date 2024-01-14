95250b592966817822e0b38d9b000b111426d73e
David Stensland <me@terite.com>
2024-01-14 15:17:52 -0800

    ag -> rg

--- fzf/setup.fish
+++ fzf/setup.fish
@@ -1,14 +1,9 @@
-if has_command fzf
-    if has_command rg
-        set -x FZF_DEFAULT_COMMAND 'rg --hidden --files'
-    else if has_command ag
-        set -x FZF_DEFAULT_COMMAND 'ag --hidden -l -g ""'
-    end
+# use ripgrep as the file listing engine
+set -x FZF_DEFAULT_COMMAND 'rg --hidden --files'
 
-    source $HOME/.fzf/shell/key-bindings.fish
-    fzf_key_bindings
+source $HOME/.fzf/shell/key-bindings.fish
+fzf_key_bindings
 
-    # bind \cp '__fzf_ctrl_t'
-    # bind \cr '__fzf_ctrl_r'
-    # bind \ec '__fzf_alt_c'
-end
+# bind \cp '__fzf_ctrl_t'
+# bind \cr '__fzf_ctrl_r'
+# bind \ec '__fzf_alt_c'
