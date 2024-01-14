b4b0c78c09059a568a0cfb163d5ca63f6df5ae7e
David Stensland <me@terite.com>
2024-01-14 15:41:39 -0800

    fzf: try disabling key bindings for now

--- fzf/setup.fish
+++ fzf/setup.fish
@@ -1,8 +1,8 @@
 # use ripgrep as the file listing engine
 set -x FZF_DEFAULT_COMMAND 'rg --hidden --files'
 
-source $HOME/.fzf/shell/key-bindings.fish
-fzf_key_bindings
+# source $HOME/.fzf/shell/key-bindings.fish
+# fzf_key_bindings
 
 # bind \cp '__fzf_ctrl_t'
 # bind \cr '__fzf_ctrl_r'
