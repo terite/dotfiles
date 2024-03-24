21a58277460282b6c30d4b001018efa218ddfa04
David Stensland <me@terite.com>
2024-03-23 19:14:49 -0700

    vim wip

--- vim/vim/init.lua
+++ vim/vim/init.lua
@@ -1,3 +1,19 @@
+--[[
+
+Cheatsheet:
+https://neovim.io/doc/user/lua-guide.html#lua-guide-vimscript
+
+Options:
+Before: set smarttab
+After:  vim.opt.smarttab = true
+
+Variables:
+Before: let g:ale_completion_enabled = 1
+After:  vim.g.ale_completion_enabled = 1
+
+]]
+
+--
 -- Do not allow files to override vim settings
 vim.opt.modeline = false
 
@@ -27,24 +43,29 @@ require('lazy').setup({
   {
     "dense-analysis/ale",
     config = function() 
-      vim.cmd([[
-        let g:ale_completion_enabled = 1
-        let g:ale_fix_on_save = 1
-        let g:ale_fixers = {
-        \    'typescript': ['prettier', 'eslint'],
-        \    'typescriptreact': ['prettier', 'eslint'],
-        \    'css': ['prettier'],
-        \}
-        nmap <leader>g :ALEGoToDefinition<CR>
-        nmap <leader>t :ALEGoToTypeDefinition<CR>
-        nmap <leader>h :ALEHover<CR>
-        nmap <leader>r :ALEFindReferences<CR>
-        nmap <silent> <C-k> <Plug>(ale_previous_wrap)
-        nmap <silent> <C-j> <Plug>(ale_next_wrap)
+      vim.g.ale_completion_enabled = 1
+      vim.g.ale_fix_on_save = 1
 
-        " https://github.com/dense-analysis/ale/issues/1700
-        set completeopt+=noinsert
-      ]])
+      vim.g.ale_fixers = {
+        javascript = { "eslint", "prettier" },
+        typescript = { "eslint", "prettier" },
+        typescriptreact = { "eslint", "prettier" },
+        css = { "prettier" },
+      }
+
+      vim.keymap.set("n", "<leader>g", ":ALEGoToDefinition<CR>")
+      vim.keymap.set("n", "<leader>t", ":ALEGoToTypeDefinition<CR>")
+      vim.keymap.set("n", "<leader>h", ":ALEHover<CR>")
+      vim.keymap.set("n", "<leader>r", ":ALEFindReferences<CR>")
+
+      vim.keymap.set("n", "<C-k>", "<Plug>(ale_previous_wrap)", { silent = true })
+      vim.keymap.set("n", "<C-j>", "<Plug>(ale_next_wrap)", { silent = true })
+
+      -- https://github.com/dense-analysis/ale/issues/1700
+      vim.opt.completeopt = vim.opt.completeopt + 'noinsert'
+
+      -- maybe later
+      -- vim.opt.omnifunc="ale#completion#OmniFunc"
     end
   },
 
@@ -62,7 +83,11 @@ require('lazy').setup({
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
+    opts = {
+    },
+    config = function()
+      vim.cmd("colorscheme tokyonight-moon")
+    end
   },
 
   {
@@ -70,6 +95,39 @@ require('lazy').setup({
     build = function()
       vim.cmd("TSUpdate")
     end,
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          "c", "lua", "vim", "vimdoc", "query",
+          "typescript", "python", "javascript"
+        },
+
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = true,
+
+        highlight = {
+          enable = true,
+
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true
+        }
+      })
+
+      vim.opt.foldmethod = 'expr'
+      vim.opt.foldexpr="nvim_treesitter#foldexpr()"
+      vim.opt.foldenable = false
+      vim.opt.foldlevel = 99
+    end
   },
 
   -- using :BD to delete a buffer but keep the window open
@@ -108,31 +166,16 @@ require('lazy').setup({
   'tpope/vim-unimpaired',
 
   -- Run :Copilot setup on new devices to get this working
-  "github/copilot.vim",
+  -- "github/copilot.vim",
 
   -- It's a frog
   {
     'tamton-aquib/duck.nvim',
     config = function()
-      vim.keymap.set('n', '<leader>dd', function() require("duck").hatch("🐸", 3) end, {})
+      vim.keymap.set('n', '<leader>dd', function() require("duck").hatch("🐸", 2) end, {})
       vim.keymap.set('n', '<leader>dk', function() require("duck").cook() end, {})
       vim.keymap.set('n', '<leader>da', function() require("duck").cook_all() end, {})
     end,
-  },
-
-  -- replaces ag.vim, provides support for :Ack term
-  {
-    "mileszs/ack.vim",
-    config = function()
-      vim.cmd([[
-        let g:ackprg = 'rg --hidden --vimgrep --smart-case'
-        cnoreabbrev ag Ack
-        cnoreabbrev aG Ack
-        cnoreabbrev Ag Ack
-        cnoreabbrev AG Ack
-        cnoreabbrev rg Ack
-      ]])
-    end
   },
 
   -- fzf https://github.com/junegunn/fzf
@@ -143,32 +186,38 @@ require('lazy').setup({
       vim.cmd[[call fzf#install()]]
     end,
     config = function()
-      vim.cmd([[
-        noremap <c-p> :FZF<cr>
-      ]])
+      vim.keymap.set("n", "<c-p>", ":FZF<cr>")
     end
   },
 })
 
+-- No swap files
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- reload when files are changed outside of vim
+vim.opt.autoread = true
+
+vim.opt.showcmd = true
+vim.opt.backspace = "indent,eol,start"
+
+-- Bash style completion.
+vim.opt.wildmenu = true
+vim.opt.wildmode = "list:longest"
+
+-- Use tab to navigate splits.
+vim.keymap.set("n", "<tab>", "<C-w><C-w>")
+vim.keymap.set("n", "<s-tab>", "<C-w><left>")
+
+vim.opt.termguicolors = true
+
+-- Show whitespace (default off for now, too distracting)
+-- set listchars=eol:¬,tab:▸\ ,trail:~,extends:>,precedes:<,space:·
+vim.opt.listchars = { eol = "¬", tab = "▸ ", trail = "~", extends = ">", precedes = "<", space = "·" }
+vim.opt.list = false
+vim.keymap.set("n", "<leader>l", ":set list!<CR>")
+
 vim.cmd([[
-
-" Searching
-set ignorecase smartcase
-set incsearch
-set hlsearch
-nnoremap <ESC><ESC> :nohlsearch<cr>
-
-" No swap files
-set noswapfile
-set nobackup
-set autoread "reload files changed outside of vim
-
-set showcmd
-set backspace=indent,eol,start
-
-" Bash style completion.
-set wildmode=list:longest
-set wildmenu
 
 " Give up and admit that I hold shift for too long
 cab Wq wq
@@ -179,30 +228,7 @@ cab Vs vs
 cab VS vs
 cab Qa qa
 cab QA qa
-
-" set colorcolumn=101
-
-" Use tab to navigate splits.
-nnoremap <tab> <C-w><C-w>
-nnoremap <s-tab> <C-w><left>
-
-" Color scheme
-" set background=dark
-set termguicolors
-colorscheme tokyonight-night
-" colorscheme luna
-" colorscheme Tomorrow-Night-Eighties
-" colorscheme solarized8_dark_high
-" colorscheme brogrammer
-" highlight ColorColumn ctermbg=8
-
-" Show whitespace (default off for now, too distracting)
-set listchars=eol:¬,tab:▸\ ,trail:~,extends:>,precedes:<,space:·
-nmap <leader>l :set list!<CR>
-set nolist
 ]])
-
-
 -- show line numbers
 vim.opt.number = true
 
@@ -215,3 +241,5 @@ vim.opt.expandtab = true
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+vim.api.nvim_create_user_command('Test', 'echo "It works!"', {})
---
+++ vim/vim/plugin/search.lua
@@ -0,0 +1,31 @@
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+
+vim.keymap.set('n', '<ESC><ESC>', ':nohlsearch<cr>')
+
+if vim.fn.executable('rg') == 1 then
+  vim.opt.grepprg='rg --vimgrep --hidden --smart-case --'
+end
+
+vim.api.nvim_create_user_command('Ack', function(opts)
+  local term = opts.args[1]
+  if term == nil or term == "" then
+    term = vim.fn.expand("<cword>")
+  end
+  print(vim.inspect(term))
+
+  vim.cmd('silent grep! ' .. term)
+  vim.cmd.copen()
+end, { nargs = '?' })
+
+
+vim.cmd([[
+  cnoreabbrev ag Ack
+  cnoreabbrev aG Ack
+  cnoreabbrev Ag Ack
+  cnoreabbrev AG Ack
+  cnoreabbrev rg Ack
+]])
