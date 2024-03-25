b7a210a8e406c2396e59317a0fc20cfb168cde80
David Stensland <me@terite.com>
2024-03-25 10:23:45 -0700

    vim: more language servers

--- vim/vim/lua/plugins/lspconfig.lua
+++ vim/vim/lua/plugins/lspconfig.lua
@@ -8,9 +8,49 @@ return {
 
       -- https://quick-lint-js.com/install/
       lspconfig.quick_lint_js.setup({})
-    
+
       -- npm install -g @vtsls/language-server
       lspconfig.vtsls.setup({})
+
+      -- npm i -g vscode-langservers-extracted
+      lspconfig.eslint.setup({})
+      lspconfig.html.setup({})
+      lspconfig.jsonls.setup({})
+      lspconfig.cssls.setup{}
+
+      -- https://luals.github.io/#neovim-install
+      -- config from https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+      lspconfig.lua_ls.setup{
+        on_init = function(client)
+          local path = client.workspace_folders[1].name
+          if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+            return
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = 'LuaJIT'
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME
+                -- Depending on the usage, you might want to add additional paths here.
+                -- "${3rd}/luv/library"
+                -- "${3rd}/busted/library",
+              }
+              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+              -- library = vim.api.nvim_get_runtime_file("", true)
+            }
+          })
+        end,
+        settings = {
+          Lua = {}
+        }
+      }
 
       -- from ale
       vim.keymap.set("n", "<C-k>", vim.diagnostic.goto_prev, { silent = true })
