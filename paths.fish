f6d9f29bd612864570f01abb5b31dd8c2b5925e5
David Stensland <me@terite.com>
2024-07-29 08:34:11 -0700

    cleanup node/python paths

--- paths.fish
+++ paths.fish
@@ -7,11 +7,12 @@ _add_path "$HOME/.local/bin"
 _add_path "$HOME/.cargo/bin"
 
 # Node
-_add_path "/usr/local/opt/node@8/bin"
 _add_path "$HOME/.npm-packages/bin"
-
-# Python
-_add_path "$HOME/Library/Python/3.9/bin"
 
 # fzf
 _add_path "$HOME/.fzf/bin"
+
+# isolated locals
+for path in $HOME/isolocal/*/bin;
+  _add_path "$path"
+end
