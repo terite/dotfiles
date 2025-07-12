a0818a975c9c100576efbb01082f83d2404a63a8
David Stensland <me@terite.com>
2025-07-12 16:52:38 -0700

    vim: only setup LSPs that are installed

--- vim/vim/lua/plugins/lspconfig.lua
+++ vim/vim/lua/plugins/lspconfig.lua
@@ -50,63 +50,82 @@ return {
     -- enabled = false,
     config = function()
       local lspconfig = require('lspconfig')
-      lspconfig.pyright.setup {}
+
+      if vim.fn.executable('pyright') == 1 then
+        lspconfig.pyright.setup {}
+      end
 
       -- https://quick-lint-js.com/install/
-      lspconfig.quick_lint_js.setup({})
+      if vim.fn.executable('quick-lint-js') == 1 then
+        lspconfig.quick_lint_js.setup({})
+      end
 
       -- npm install -g @vtsls/language-server
-      lspconfig.vtsls.setup({})
+      if vim.fn.executable('vtsls') == 1 then
+        lspconfig.vtsls.setup({})
+      end
 
       -- npm i -g vscode-langservers-extracted
-      lspconfig.eslint.setup({})
-      lspconfig.html.setup({})
-      lspconfig.jsonls.setup({})
-      lspconfig.cssls.setup{}
+      if vim.fn.executable('vscode-html-language-server') == 1 then
+        lspconfig.html.setup({})
+      end
+      if vim.fn.executable('vscode-css-language-server') == 1 then
+        lspconfig.cssls.setup{}
+      end
+      if vim.fn.executable('vscode-json-language-server') == 1 then
+        lspconfig.jsonls.setup({})
+      end
+      if vim.fn.executable('vscode-eslint-language-server') == 1 then
+        lspconfig.eslint.setup({})
+      end
 
       -- pipx install python-lsp-server
       -- pipx install python-lsp-black
-      lspconfig.pylsp.setup({
-        plugins = {
-          flake8 = {
-            enabled = true,
+      if vim.fn.executable('pylsp') == 1 then
+        lspconfig.pylsp.setup({
+          plugins = {
+            flake8 = {
+              enabled = true,
+            },
           },
-        },
-      })
+        })
+      end
 
       -- https://luals.github.io/#neovim-install
       -- config from https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
-      lspconfig.lua_ls.setup{
-        on_init = function(client)
-          local path = client.workspace_folders[1].name
-          if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
-            return
-          end
+      if vim.fn.executable('lua-language-server') == 1 then
+        lspconfig.lua_ls.setup{
+          on_init = function(client)
+            local path = client.workspace_folders[1].name
+            if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+              return
+            end
 
-          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-            runtime = {
-              -- Tell the language server which version of Lua you're using
-              -- (most likely LuaJIT in the case of Neovim)
-              version = 'LuaJIT'
-            },
-            -- Make the server aware of Neovim runtime files
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                vim.env.VIMRUNTIME
-                -- Depending on the usage, you might want to add additional paths here.
-                -- "${3rd}/luv/library"
-                -- "${3rd}/busted/library",
+            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+              runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT'
+              },
+              -- Make the server aware of Neovim runtime files
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME
+                  -- Depending on the usage, you might want to add additional paths here.
+                  -- "${3rd}/luv/library"
+                  -- "${3rd}/busted/library",
+                }
+                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                -- library = vim.api.nvim_get_runtime_file("", true)
               }
-              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-              -- library = vim.api.nvim_get_runtime_file("", true)
-            }
-          })
-        end,
-        settings = {
-          Lua = {}
+            })
+          end,
+          settings = {
+            Lua = {}
+          }
         }
-      }
+      end
       globalkeys()
     end
   }
